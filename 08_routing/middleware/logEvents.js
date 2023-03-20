383cb148083a8407e20f91fbcf3a71280f95ab9a
valenzsa <valenzsa418@gmail.com>
2023-03-19 23:54:09 -0400

    Working with routing and test out CRUD operations using Thunder Client VS Code Extension

---
+++ 08_routing/middleware/logEvents.js
@@ -0,0 +1,36 @@
+const { format } = require('date-fns');
+const { v4: uuid } = require('uuid');
+
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const logEvents = async (message, logName) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+    try {
+        // Check if /logs directory doesn't exists
+        // if so, create it
+        if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
+        }
+
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const logger = (req, res, next) => {
+    // req.headers.origin - where the request is coming, what website sent it to us
+    // req.url - what url was requested.
+    logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+    console.log(`${req.method} ${req.path}`);
+    next();
+}
+
+module.exports = {
+    logEvents,
+    logger
+}
---
+++ 08_routing/server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
+//const logEvents = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const PORT = process.env.PORT || 3500;
+
+// Middlewares
+// There are three typess of Middlewares
+// 1.) Built-in middleware
+// 2.) Custom middleware
+// 3.) Middleware from third parties
+
+// // Custom middleware logger
+app.use(logger);
+// app.use((req, res, next) => {
+//     // req.headers.origin - where the request is coming, what website sent it to us
+//     // req.url - what url was requested.
+//     logEvents(`${req.method}\t${req.headers.origin}\t${req.url}`, 'reqLog.txt');
+//     console.log(`${req.method} ${req.path}`);
+//     next();
+// });
+
+// Third party middleware
+// Cross Origin Resource Sharing
+const whitelist = ['https://www.yoursite.com', 'https://yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    // Args
+    // origin - site that requested this site
+    // !origin - undefined/false (site isn't in the whitelist, therefore its undefined)
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded data
+// in other words, form data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+app.use('/subdir', express.static(path.join(__dirname, '/public')));
+
+// routes
+app.use('/', require('./routes/root'));
+app.use('/subdir', require('./routes/subdir'));
+app.use('/employees', require('./routes/api/employees'));
+
+//app.use('/')
+// app.get('/*', (req, res) => {
+//     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+// });
+app.all('*', (req, res) => {
+    res.status(404);
+    // Check different types that can be accepted
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'));
+    } else if (req.accepts('json')) {
+        res.json({ error: "404 Not Found" });
+    } else {
+        res.type('txt').send("404 Not Found");
+    }
+});
+
+
+// app.use(function (err, req, res, next) {
+//     console.error(err.stack);
+//     res.status(500).send(err.message);
+// });
+
+app.use(errorHandler);
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
